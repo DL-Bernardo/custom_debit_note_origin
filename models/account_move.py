e1e29c9a70b1fe63fe2e0d51874d0e09a2db3dac
DL-Bernardo <bvieira@digitalub.ao>
2025-09-06 11:59:03 +0100

    Alterações no Modulo

--- models/account_move.py
+++ models/account_move.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -7,8 +8,6 @@ class AccountMove(models.Model):
         'account.move',
         string='Fatura de Origem',
         domain="[('move_type', '=', 'out_invoice'), ('partner_id', '=', partner_id), ('state', '=', 'posted')]",
-        readonly=True,
-        states={'draft': [('readonly', False)]},
         help="Selecione a fatura de origem associada ao cliente."
     )
 
@@ -17,24 +16,9 @@ class AccountMove(models.Model):
         """Limpa o campo origin_invoice_id quando o cliente muda."""
         self.origin_invoice_id = False
 
-    @api.onchange('journal_id')
-    def _onchange_journal_id(self):
-        """Mostra o campo origin_invoice_id apenas para o diário ND Nota de Débito."""
-        if self.journal_id and self.journal_id.name == 'ND Nota de Débito':
-            self.origin_invoice_id = False  # Limpa o campo ao mudar para este diário
-        else:
-            self.origin_invoice_id = False  # Garante que o campo seja limpo se o diário for diferente
-
-    @api.constrains('journal_id', 'origin_invoice_id')
-    def _check_origin_invoice_required(self):
-        """Validação para garantir que o campo Fatura de Origem seja preenchido com o diário ND."""
-        for record in self:
-            if record.journal_id and record.journal_id.name == 'ND Nota de Débito' and not record.origin_invoice_id:
-                raise ValidationError("Erro de Validação! Com o diário 'ND Nota de Débito' selecionado, o campo 'Fatura de Origem' na nota de débito deve ser preenchido.")
-
     def action_post(self):
         """Sobrescreve a validação de postagem para incluir a lógica personalizada."""
         for move in self:
-            if move.journal_id and move.journal_id.name == 'ND Nota de Débito' and not move.origin_invoice_id:
-                raise ValidationError("Erro de Validação! Com o diário 'ND Nota de Débito' selecionado, o campo 'Fatura de Origem' na nota de débito deve ser preenchido.")
+            if move.journal_id and move.journal_id.code == 'ND' and not move.origin_invoice_id:
+                raise ValidationError("Com o diário 'Nota de Débito' selecionado, o campo 'Fatura de Origem' na nota de débito deve ser preenchido.")
         return super(AccountMove, self).action_post()
